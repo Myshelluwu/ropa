28ce8858f37cba9457128149c56b0211b5549af2
Michelle Sanchez <myshellsanchezbarba@gmail.com>
2023-10-25 22:15:23 -0600

    Javascript

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function cambiarTexto(){
+    document.getElementById("text").innerHTML="Nuestra ropa es mucho más que simples prendas de vestir; es una expresión de tu estilo y personalidad. En cada pieza que diseñamos y seleccionamos, nos esforzamos por ofrecer calidad, comodidad y moda.";
+};
+
+function cambiarTexto2(){
+    document.getElementById("text").innerHTML="Nuestro calzado es mucho más que un simple par de zapatos; es la base de tu estilo, comodidad y confianza. En cada paso que das, queremos que sientas la calidad y la atención al detalle que ponemos en cada par que ofrecemos.";
+};
